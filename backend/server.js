a4533f4b96ffd958680ac762d064a5022cf6834f
Azis Adi Kuncoro <azisadikuncoro@gmail.com>
2018-06-05 20:28:06 +0700

    Done add API for trending words

--- backend/server.js
+++ backend/server.js
@@ -96,6 +96,39 @@ router.get('/words/:date', (req,res) => {
 
   
 })
+
+router.get('/trend/:since/:until/:word', (req,res) => {
+  
+  const cache_key = req.url;
+  console.log(`cache_key = ${req.url}`);
+  const TTL = 10*60; // 10 minutes
+  
+  
+  client.get(cache_key, function(err, data){
+    if (data){
+      console.log("Cached");
+      return res.json(JSON.parse(data));
+    }
+    else {
+      console.log("Not Cached");
+      const start_date = req.params.since;
+      const end_date = req.params.until;
+      const word = req.params.word;
+      
+      getTrendWords(start_date, end_date, word).then((result) => {
+        client.setex(cache_key, TTL, JSON.stringify(result));
+        return res.json(result);
+      }, (err) => {
+        return res.json({success:false, error: err});
+      });
+      
+    }
+
+  })
+
+  
+})
+
 router.get('/frequent-poster/forum/:date', (req,res) => {
   
   const date_key = req.url;
@@ -114,7 +147,7 @@ router.get('/frequent-poster/forum/:date', (req,res) => {
       const tables = {
         'thread':`[kaskus-166400:intern_dataset.thread_${date}]`,
         'forum':`[kaskus-166400:intern_dataset.forum]`,
-        'post':`[[kaskus-166400:intern_dataset.post_${date}]`
+        'post':`[kaskus-166400:intern_dataset.post_${date}]`
       };
       
       getPerForumFrequentPoster(tables, date_key).then((result) => {
@@ -135,32 +168,7 @@ router.get('/frequent-poster/forum/:date', (req,res) => {
 
 // === Query Function ===
 async function getWords(table) {
-  // [kaskus-166400:intern_dataset.post_20180527]
-  const sqlQuery = `SELECT page_text FROM ${table} LIMIT 100`;
-
-  // Query options list: https://cloud.google.com/bigquery/docs/reference/v2/jobs/query
-  const options = {
-    query: sqlQuery,
-    useLegacySql: true, // Use Legacy SQL syntax for queries.
-  };
-
-  // Runs the query
-  var to_return = await bigquery
-  .query(options)
-  .then(results => {
-    const freq_posters = results[0];
-    return ({
-      success: true,
-      data: freq_posters
-    });
-  })
-  .catch(err => {
-    return ({
-      success: false,
-      error: err.errors
-    });
-  });
-  return to_return;
+  // TODO
 
 }
 
@@ -174,36 +182,36 @@ async function getPerForumFrequentPoster(tables){
   name as forum_name,
   post_username,
   thread_count
-FROM (
-  SELECT
-    thread.forum_id,
-    name,
-    post.post_username,
-    COUNT(*) AS thread_count,
-    ROW_NUMBER() OVER(PARTITION BY thread.forum_id ORDER BY thread_count DESC) AS rownum
-  FROM
-    ${tables['thread']} thread
-  INNER JOIN
-    ${tables['forum']} forum
-  ON
-    thread.forum_id = forum.forum_id
-  INNER JOIN
-    ${tables['post']} post
-  ON
-    thread.id = post.thread_id
-  GROUP BY
-    thread.forum_id,
-    name,
-    post.post_username
-  ORDER BY
-    thread.forum_id,
-    thread_count DESC,
-    post.post_username ASC,
-    ) TEMP
-WHERE
-  rownum = 1
-LIMIT
-  1000;`;
+  FROM (
+    SELECT
+      thread.forum_id,
+      name,
+      post.post_username,
+      COUNT(*) AS thread_count,
+      ROW_NUMBER() OVER(PARTITION BY thread.forum_id ORDER BY thread_count DESC) AS rownum
+    FROM
+      ${tables['thread']} thread
+    INNER JOIN
+      ${tables['forum']} forum
+    ON
+      thread.forum_id = forum.forum_id
+    INNER JOIN
+      ${tables['post']} post
+    ON
+      thread.id = post.thread_id
+    GROUP BY
+      thread.forum_id,
+      name,
+      post.post_username
+    ORDER BY
+      thread.forum_id,
+      thread_count DESC,
+      post.post_username ASC,
+      ) TEMP
+  WHERE
+    rownum = 1
+  LIMIT
+    1000;`;
   
   // Query options list: https://cloud.google.com/bigquery/docs/reference/v2/jobs/query
   const options = {
@@ -236,14 +244,14 @@ async function getGlobalFrequentPoster(table){
   SELECT
   post_username,
   COUNT(*) AS thread_count
-FROM
-  ${table}
-GROUP BY
-  post_username
-ORDER BY
-  thread_count DESC
-LIMIT
-  1;`;
+  FROM
+    ${table}
+  GROUP BY
+    post_username
+  ORDER BY
+    thread_count DESC
+  LIMIT
+    1;`;
 
   // Query options list: https://cloud.google.com/bigquery/docs/reference/v2/jobs/query
   const options = {
@@ -270,6 +278,63 @@ LIMIT
   });
   return to_return;
   
+}
+
+
+async function getTrendWords(start_date, end_date, word) {
+  
+  const table_name = 'kaskus-166400.intern_dataset.post_';
+  const start_table_date = start_date.split("-").join("");
+  const end_table_date = end_date.split("-").join("");
+  
+  const sqlQuery =`
+    CREATE TEMP FUNCTION countWordInSentence(sentence string, key string )
+  RETURNS INT64
+  AS(
+    (SELECT COUNT(*)
+    FROM UNNEST(SPLIT(REGEXP_REPLACE(REGEXP_REPLACE(sentence,"\[.*?\]"," "),"[ ]+[ ]"," ")," ")) as word
+    GROUP BY word
+    HAVING word = key)
+  );
+
+  SELECT
+    date,
+    SUM(countWordInSentence(sentence,'${word}')) as counted_word  
+  FROM (
+    SELECT
+    CAST(EXTRACT(DATE FROM TIMESTAMP_SECONDS(dateline))AS STRING) as date,
+    page_text as sentence
+    FROM \`kaskus-166400.intern_dataset.post_*\`
+    WHERE _TABLE_SUFFIX BETWEEN '${start_table_date}' and '${end_table_date}')
+  WHERE date >= '${start_date}'
+  GROUP BY date
+  ORDER BY date ASC;
+  `;
+  console.log('Executed....');
+    
+  // Query options list: https://cloud.google.com/bigquery/docs/reference/v2/jobs/query
+  const options = {
+    query: sqlQuery,
+    useLegacySql: false, // Use Standard SQL syntax for queries.
+  };
+
+  // Runs the query
+  var to_return = await bigquery
+  .query(options)
+  .then(results => {
+    const rows = results[0];
+    console.log("query success");
+    console.log(rows);
+    return ({
+      success: true,
+      data: rows
+    });
+  })
+  .catch(err => {
+    console.log("query fail");
+    throw err.errors;
+  });
+  return to_return;
 }
 
 // =====
