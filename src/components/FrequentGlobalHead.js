58777cfb2230dd1006c8e39563830adc0afff637
rizkiduwinanto <rizkiduwinanto@gmail.com>
2018-06-22 16:30:39 +0700

    add tablecell

--- src/components/FrequentGlobalHead.js
+++ src/components/FrequentGlobalHead.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { TableRow, TableHead, TableCell, Typography } from '@material-ui/core';
+import { TableRow, TableHead, Typography } from '@material-ui/core';
+import CustomTableCell from './CustomTableCell';
 
 class FrequentGlobalHead extends React.Component {
   render(){
     return (
       <TableHead>
         <TableRow>
-          <TableCell><Typography>Post Username</Typography></TableCell>
-          <TableCell><Typography>Count</Typography></TableCell>
+          <CustomTableCell><Typography>Post Username</Typography></CustomTableCell>
+          <CustomTableCell><Typography>Count</Typography></CustomTableCell>
         </TableRow>
       </TableHead>
     );
--- src/containers/FrequentGlobal.js
+++ src/containers/FrequentGlobal.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { Table, Typography, Paper, Button } from '@material-ui/core';
 import ListFrequentGlobal from './ListFrequentGlobal';
 import FrequentGlobalHead from '../components/FrequentGlobalHead';
-import DatePicker from 'react-date-picker';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import 'react-day-picker/lib/style.css';
 import { updateFrequentGlobal } from '../actions/frequentGlobalAction';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import FrequentNavTabs from '../components/FrequentNavTabs';
+
+const DatePicker = (props) => {
+  return (
+    <div className="text-center py-3">
+          <div className="d-block">
+            <span className="d-inline-block pr-3">{props.label}</span>
+            <span className="text-center">
+            <DayPickerInput onDayChange={props.handleChange} value={props.date}/>
+            </span>
+            <span role="img" aria-label="calendar-emoji" style={{fontSize: '25px'}} className="d-inline-block pl-3"> 📅 </span>
+          </div>
+        </div>
+  );
+}
 
 class FrequentGlobal extends React.Component {
   constructor(props) {
@@ -36,10 +51,12 @@ class FrequentGlobal extends React.Component {
     return (
       <Paper>
         <FrequentNavTabs/>
-        <Typography variant="title" >Frequent Global</Typography>
-        <DatePicker onChange={this.handleSince} value={this.state.since}/>
-        <DatePicker onChange={this.handleUntil} value={this.state.until}/>
-        <Button color="primary" onClick={this.handleChange}>Show!</Button>
+        <Typography className="text-center py-3" variant="title" >Frequent Global</Typography>
+        <DatePicker label='Start Date' handleChange={this.handleSince} date={this.state.since} />
+        <DatePicker label='End Date' handleChange={this.handleUntil} date={this.state.until} />
+        <div className="text-center">
+          <Button color="primary" onClick={this.handleChange}>Show!</Button>
+        </div>
         <Table>
           <FrequentGlobalHead />
           <ListFrequentGlobal />
