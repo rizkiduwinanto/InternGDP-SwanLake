1c8e6678873675970dd78ffb8da81a7f3c1c621b
rizkiduwinanto <rizkiduwinanto@gmail.com>
2018-07-02 10:51:51 +0700

    add loading in forum list

---
+++ src/containers/ForumSelector.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Paper, List, ListItem, ListItemText } from '@material-ui/core';
+import { connect } from 'react-redux';
+import { fetchForumList } from '../actions/forumAction';
+import { updateForum } from '../actions/frequentPerForumAction'
+import Spinner from '../components/Spinner';
+
+class ForumSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      forum: null,
+      loading: false,
+    };
+    this.handleForum = this.handleForum.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    this.props.fetchForumList();
+  }
+  
+  componentWillReceiveProps() {
+    this.setState({ loading: false });
+  }
+
+  handleForum(forum) {
+    this.setState({ forum : forum });
+    this.props.updateForum(forum);
+  }
+
+  render() {
+    const showSpinnerWhenLoading = () => (this.state.loading) ? <Spinner /> : <List>{rows}</List>;
+    const rows = [];
+    if (this.props.forum_list.data != null) {
+      this.props.forum_list.data.forEach((forum) => {
+        rows.push(
+          <ListItem button key={forum.forum_id} onClick={() => this.handleForum(forum)}>
+            <ListItemText primary={forum.forum_name}/>
+          </ListItem>);
+      });
+    }
+
+    return (
+      <Paper style={{maxHeight: 200, maxWidth: '80%', overflow: 'auto'}}>
+        {showSpinnerWhenLoading()}
+      </Paper>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    forum_list : state.frequent.forumList
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchForumList: () => dispatch(fetchForumList()),
+    updateForum: (forum) => dispatch(updateForum(forum))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ForumSelector);
+
--- src/reducers/reducerFrequent.js
+++ src/reducers/reducerFrequent.js
@@ -6,7 +6,7 @@ const initialState = {
   until_global: new Date(),
   since_perforum: new Date(), 
   until_perforum: new Date(),
-  forum : {},
+  forum : null,
   limit_global: 0,
   limit_perforum:0
 }
@@ -23,6 +23,8 @@ export default function (state = initialState, action) {
       return {...state, since_global: action.since, until_global: action.until, limit_global: action.limit};
     case 'UPDATE_FREQUENT_PERFORUM':
       return {...state, since_perforum: action.since, until_perforum: action.until, limit_perforum: action.limit, forum: action.forum};
+    case 'UPDATE_FORUM':
+      return {...state, forum: action.forum};
     default:
       return state;
   }
