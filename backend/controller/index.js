19ad879bd1ab63b5f49f32c6a6370e873c9e6c32
Azis Adi Kuncoro <azisadikuncoro@gmail.com>
2018-07-16 11:22:04 +0700

    Register the 2 new endpoints

--- backend/controller/index.js
+++ backend/controller/index.js
@@ -3,9 +3,12 @@ import words from './api/words';
 import trend from './api/trend';
 import frequent_poster from './api/frequent_poster';
 import forum_list from './api/forum_list';
+import mail_keywords from './api/mail_keywords';
+import keyword_mail_addr from './api/keyword_mail_addr';
+
 
 const app = express();
 
-app.use('/api',[words, trend, frequent_poster, forum_list]);
+app.use('/api',[words, trend, frequent_poster, forum_list, mail_keywords, keyword_mail_addr]);
 
 module.exports = app; 
